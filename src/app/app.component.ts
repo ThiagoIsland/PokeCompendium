5b0a59ad7cac3501a8e5b44e72804b226b3f0770
Thiago Alessandro <114539167+ThiagoIsland@users.noreply.github.com>
2025-01-29 22:48:28 -0300

    Adding footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,17 @@ import { RouterModule } from '@angular/router';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { FooterComponent } from './footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, RouterLink, RouterLinkActive, HeaderComponent, SideMenuComponent, MatButtonModule, MatSidenavModule],
+  imports: 
+    [RouterOutlet, RouterModule, RouterLink, RouterLinkActive, 
+    HeaderComponent, SideMenuComponent, FooterComponent, 
+    MatButtonModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
